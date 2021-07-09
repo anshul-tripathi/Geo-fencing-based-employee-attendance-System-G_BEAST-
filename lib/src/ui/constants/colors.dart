cff7eb0d6a47c70ea0ad1b8b05b9494b78d40aea
Anshul Tripathi <anshultripathi265401@gmail.com>
2021-07-10 00:50:29 +0530

    push

---
+++ lib/src/ui/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const dashBoardColor = Color.fromRGBO(72, 128, 209, 1);
+const splashScreenColorBottom = Color.fromRGBO(72, 128, 209, 1);
+const splashScreenColorTop= Color.fromRGBO(72, 128, 209, 0.6);
+const appbarcolor=Color.fromRGBO(72, 128, 209, 1);
+const leaveCardcolor =  Color(0xFF8685E5);
